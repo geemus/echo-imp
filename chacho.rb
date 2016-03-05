f04c7cb23ca1b0aa72369bcc15069e7489ed2fe9
geemus <geemus@gmail.com>
2016-03-05 10:21:55 -0600

    initial setup

---
+++ chacho.rb
@@ -0,0 +1,48 @@
+require 'bundler'
+Bundler.require
+
+require 'json'
+
+class Chacho < Sinatra::Base
+
+  use Rack::Auth::Basic, "Restricted Area" do |username, password|
+    ENV['CREDENTIALS'].split(",").map {|cred| Rack::Utils.secure_compare(cred, "#{username}:#{password}")}.include?(true)
+  end
+
+  post '/' do
+    data = JSON.parse(request.body.read)
+    # requestId, timestamp, type, ...
+    case data['request']['type']
+    when 'LaunchRequest'
+    when 'IntentRequest'
+      # intent => { name, slots => { string => { name => string, value => string } } }
+    when 'SessionEndedRequest'
+      # reason
+    end
+
+    status(200)
+    body({
+      version: '1.0',
+      # session: {} # key/value pairs to set
+      response: {
+        card: {
+          content:  'Hello World',
+          title:    'Hello World',
+          type:     'Simple'
+        },
+        outputSpeech: {
+          text: 'Hello World',
+          type: 'PlainText'
+        },
+        reprompt: {
+          outputSpeech: {
+            text: 'Can I help you with anything else?',
+            type: 'PlainText'
+          }
+        },
+        shouldEndSession: true
+      }
+    }).to_json
+  end
+
+end
---
+++ config.ru
@@ -0,0 +1,2 @@
+require './chacho'
+run Chacho
